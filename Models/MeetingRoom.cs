ebe022acc28a63581e966fd9ca9639ceb09dd21b
Saif Madie <saifatyaif@gmail.com>
2026-04-27 10:14:23 +0200

    meetingroom tilføjet

--- Models/MeetingRoom.cs
+++ Models/MeetingRoom.cs
@@ -3,5 +3,31 @@ namespace DSVMeetingRoomBooking.Models
 	public class MeetingRoom
 	{
 
-	}
+		private int _RoomId { get; set; }
+		
+        private string _NameOfRoom { get; set; }
+
+		private bool _IsAvailable { get; set; }
+
+		private int _Capacity { get; set; }
+
+
+		public MeetingRoom()
+		{
+
+
+
+		}
+
+		//public MeetingRoom ( int roomid , string nameOfRoom , bool isavailble , int capacity)
+		//{
+		//	RoomId = roomid;
+		//	nameOfRoom = nameOfRoom;
+		//	IsAvailable = isavailble;
+		//	_Capacity = capacity;
+
+
+
+		//}
+    }
 }
