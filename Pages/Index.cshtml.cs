7dd0c159abf00bf20d8c5f0b567472ecf561657d
rchmeis <r.chmeis@gmail.com>
2026-04-27 10:15:59 +0200

    index og createbooking cshtml

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using DSVMeetingRoomBooking.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +6,17 @@ namespace DSVMeetingRoomBooking.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly MeetingRoomService _meetingRoomService;
+        public IndexModel(MeetingRoomService meetingRoomService)
+        {
+            _meetingRoomService = meetingRoomService;
+        }
         public void OnGet()
         {
 
         }
-    }
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("/Index");
+        }
 }
