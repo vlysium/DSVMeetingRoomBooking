d8bed8f137bdefc039812522bdfa8c2f372003d0
rchmeis <r.chmeis@gmail.com>
2026-04-28 00:18:25 +0200

    Search by employeeId lavet på index, den redirecter (via OnPost) til Bookings.cshtml og giver et employeeId med til Onpost der.

--- Pages/Bookings.cshtml.cs
+++ Pages/Bookings.cshtml.cs
@@ -7,17 +7,26 @@ namespace DSVMeetingRoomBooking.Pages
 {
     public class BookingsModel : PageModel
     {
-        public List<Booking> bookings;
-        private readonly BookingService _bookingservice;
+        public List<Booking> Bookings { get; set; }
+        private readonly BookingService _bookingService;
         public BookingsModel(BookingService bookingservice)
         {
-            _bookingservice = bookingservice;
+            _bookingService = bookingservice;
         }
-
-       
+                
         public void OnGet()
         {
-            //bookings= _bookingservice.FILTRETINGSfunktion();
+            
+        }
+        public void OnGet(string employeeId)
+        {
+            //Bookings = _bookingService.GetBookingsByEmployeeId(employeeId); 
+
+            //i bookingrepository skal der laves en GetBookingsByEmployeeId så virker koden.
+            //så skal en funktion skrives (foreach booking...) så brugerens MeetingRooms gøres til en liste
+            //der der kan bruges til visning i bookings.cshtml            
+         
         }
     }
 }
+
